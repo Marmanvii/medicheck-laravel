336cea8578d3ba869fd10798ebc84a13fd3297e9
Marmanvii <marmanvi@gmail.com>
2018-09-15 23:57:49 -0300

    User can now add an appointment

--- resources/views/appointments/create.blade.php
+++ resources/views/appointments/create.blade.php
@@ -10,25 +10,32 @@
         <input type="number" class="form-control" id="telefono" name="telefono" placeholder="91213420" value="{{ old('telefono') }}" required>
       </div>
       <div class="form-group col-md-8">
-        <label for="telefono">placeholder</label>
-        <input type="text" class="form-control" id="placeholder" name="placeholder" placeholder="placeholder" value="{{ old('telefono') }}" required>
+        <label for="telefono">Médico</label>
+        <select id="medics_id" class="form-control" name="medics_id" required>
+          <option value="" disabled selected>Seleccione un médico...</option>
+          @foreach($users as $medic) <!--Función de laravel, se busca por fila de la tabla, adquiriendo un medico y sus datos por ciclo, se obtiene desde el controlador, "as" sirve a modo de alias-->
+            @if($medic->type=='medic')
+              <option value="{{$medic->id}}">{{$medic->name}} {{$medic->last_name}}: {{$medic->especialidad}}</option>
+            @endif
+          @endforeach
+        </select>
       </div>
     </div>
     <div class="col" align="center">
       <div class="form-group col-md-8">
         <label for="descripcion">Observación</label>
-        <input type="text" class="form-control" id="descripcion" name="descripcion"  value="{{ old('descripcion') }}" required>
+        <input type="textarea" class="form-control" id="observacion" name="observacion"  value="{{ old('observacion') }}" required>
       </div>
       <div class="form-group col-md-8">
-        <label for="telefono">placeholder2</label>
-        <input type="text" class="form-control" id="placeholder2" name="placeholder2" placeholder="placeholder2" value="{{ old('telefono') }}" required>
+        <label for="telefono">Fecha</label>
+        <input type="date" class="form-control" id="fecha" name="fecha" min="<?php echo date('Y-m-d');?>" required>
       </div>
     </div>
   </div>
     <input name="patient_id" type="hidden" value="{{old (Auth::id(), Auth::id())}}">
-    <input name="fecha" type="hidden" value="{{ old( 'fecha', $fecha) }}">
+    <input name="bloque" type="hidden" value="1">
     <button type="submit" align="center"class="btn">Ingresar</button>
     <a class="btn btn-outline-light" role="main" href="/appointments">Regresar</a>
   </form>
-
+@include('layout.errors')
 @endsection
