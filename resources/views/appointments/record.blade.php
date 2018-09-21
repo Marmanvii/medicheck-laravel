a4d1d4c11488add5154d963e419f5907a8d09b75
ClaudioDuran <claudiodurannunez13@gmail.com>
2018-09-21 01:38:28 -0300

    Fixed medication show

--- resources/views/appointments/record.blade.php
+++ resources/views/appointments/record.blade.php
@@ -12,24 +12,38 @@
                 <h5 class="card-subtitle" card text>{{$file->tratamiento}}</h5>
                 <h6 class="card-text" card text>{{$file->descripcion}}</h6>
                 <h6 class="card-text" card text>{{$appointment->fecha}}</h6>
+                <?php
+                  $i = 0;
+                ?>
+                @foreach ($medications as $medication) <!-- Se verifica si hay medicamentos asociados a la cita-->
+                  @if($medication->appointment_id == $appointment->id)
+                    <?php
+                      $i = 1;
+                    ?>
+                  @endif
+                @endforeach
                 <table class="table table-sm" style="width:80%; margin:0px auto;">
                   <thead>
                     <tr>
-                      <th scope="col">Nombre</th>
-                      <th scope="col">Intervalo</th>
-                      <th scope="col">Duración</th>
+                      @if ($i==1) <!-- Solo si hay medicamentos se carga la tabla-->
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Intervalo</th>
+                        <th scope="col">Duración</th>
+                      @endif
                     </tr>
                   </thead>
                   <tbody>
-                    @foreach ($medications as $medication)
-                      @if($medication->appointment_id == $appointment->id)
-                        <tr>
-                          <td>{{$medication->nombre}}</td>
-                          <td>{{$medication->intervalo}}</td>
-                          <td>{{$medication->duracion}}</td>
-                        </tr>
-                      @endif
-                    @endforeach
+                    @if ($i==1) <!-- Solo si hay medicamentos se vuelven a recorrer-->
+                      @foreach ($medications as $medication)
+                        @if($medication->appointment_id == $appointment->id)
+                          <tr>
+                            <td>{{$medication->nombre}}</td>
+                            <td>{{$medication->intervalo}}</td>
+                            <td>{{$medication->duracion}}</td>
+                          </tr>
+                        @endif
+                      @endforeach
+                    @endif
                   </tbody>
                 </table>
                 <p class="card-text"><small class="text-muted">MediCheck</small></p>
