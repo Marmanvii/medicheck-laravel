be5d50d1e62138d9e2935275d765e7a2ce20af5a
Marmanvii <marmanvi@gmail.com>
2018-09-21 10:07:40 -0300

    User can now choose an area before choosing a medic

--- resources/views/appointments/select_area.blade.php
+++ resources/views/appointments/select_area.blade.php
@@ -1,29 +1,21 @@
 @extends('layout.master')
-@section('styles')
-@endsection
 @section('content') <!--Se define como seccion para ser incluido en la seccion contenido en el layout principal-->
-  <h1 align="center">Registrar cita</h1>
-  <h2 align="center">Seleccione su médico y fecha deseada</h2>
+  <h1 align="center">Seeccione área médica</h1>
+  <h2 align="center">Seleccione el área de salud deseada</h2>
   <div class="card" style="text-align: center;width:80%; margin:0px auto;width: 64rem;">
     <div class="card-body">
-  <form action="/appointments/bloques_disponibles" method="GET" style="text-align: center;width:80%; margin:0px auto;"><!--Luego en el controlador, con request se obtiene este dato-->
+  <form action="/appointments/select_fecha_medico" method="GET" style="text-align: center;width:80%; margin:0px auto;"><!--Luego en el controlador, con request se obtiene este dato-->
   {{ csrf_field() }} <!--Sirve para evitar ataques modificando el html desde el navegador.-->
   <div class="form-row">
     <div class="col" align="center">
       <div class="form-group col-md-8">
         <label for="telefono">Médico</label>
-        <select id="medics_id" class="form-control" name="medics_id" required>
-          <option value="" disabled selected>Seleccione un médico</option>
+        <select id="especialidad" class="form-control" name="especialidad" required>
+          <option value="" disabled selected>Seleccione una especialidad</option>
           @foreach($medics as $medic) <!--Función de laravel, se busca por fila de la tabla, adquiriendo un medico y sus datos por ciclo, se obtiene desde el controlador, "as" sirve a modo de alias-->
-              <option value="{{$medic->id}}">{{$medic->name}} {{$medic->last_name}}</option>
+              <option value="{{$medic->especialidad}}">{{$medic->especialidad}}</option>
           @endforeach
         </select>
-      </div>
-    </div>
-    <div class="col" align="center">
-      <div class="form-group col-md-8">
-        <label for="telefono">Fecha</label>
-        <input type="date" class="form-control" id="fecha" name="fecha" min="<?php echo date('Y-m-d');?>" required>
       </div>
     </div>
   </div>
