3d08d865578ad94d3fa83a51195e425459bacd29
ClaudioDuran <claudiodurannunez13@gmail.com>
2018-10-16 17:28:03 -0300

    View select date

---
+++ resources/views/appointments/select_fecha.blade.php
@@ -0,0 +1,40 @@
+@extends('layout.master')
+<?php
+$i = 1;
+?>
+@section('content')
+  <h3 class="text-center">Seleccionar Fecha</h3>
+  <table class="table table-striped table-bordered table-hover" style="width:80%; margin:0px auto; text-align:center;">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Fecha</th>
+        <th scope="col">Acción</th>
+      </tr>
+    </thead>
+  <tbody>
+    <tr>
+      <th scope="row">{{"$i"}}</th>
+      <?php
+        $i = $i + 1;
+      ?>
+      <th scope="row">Fechas</th>
+      <th scope="row">
+        <form action="/appointments/bloques_disponibles" method="POST">
+          {{csrf_field()}}
+          <input name="appointment_id" type="hidden" value="">
+          <button class="btn btn-dark btn-sm" type="submit">Seleccionar Fecha</button>
+        </form>
+      </th>
+    </tr>
+
+
+  </tbody>
+  </table>
+<main role="main" class="inner cover text-center">
+  <h3></h3>
+  <p class="lead">
+    <a href="/appointments" class="btn btn-lg btn-secondary">Volver</a>
+  </p>
+</main>
+@endsection
