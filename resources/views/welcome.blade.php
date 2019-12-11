d22adf20241177465d994bfc14b96c13375bd792
Claudio Durán <claudiodurannunez13@gmail.com>
2019-12-11 00:28:04 -0300

    carousel ready

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,105 +1,101 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-        <title>MediCheck</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-        @import url(https://fonts.googleapis.com/css?family=Raleway:400,,800,900);
-        html {
-          width: 100%;
-          height: 100%;
-        }
-
-        body {
-          background: -webkit-linear-gradient(#ebece7, white);
-          background: linear-gradient(#ebece7, white);
-          margin: 0;
-          width: 100%;
-          height: 100%;
-          font-family: 'Raleway', sans-serif;
-        }
-
-        .container {
-          position: absolute;
-          -webkit-transform: translate(-50%, -50%);
-          -ms-transform: translate(-50%, -50%);
-          transform: translate(-50%, -50%);
-          left: 50%;
-          top: 50%;
-        }
-
-        .title {
-          font-weight: 800;
-          color: transparent;
-          font-size: 200px;
-          background: url("/images/doc1.jpg") repeat;
-          background-position: 40% 50%;
-          -webkit-background-clip: text;
-          position: relative;
-          padding-bottom: 50px;
-          text-align: center;
-          line-height: 150px;
-          letter-spacing: -8px;
-        }
-
-        .subtitle {
-          display: block;
-          text-align: center;
-          text-transform: uppercase;
-          padding-top: 10px;
-        }
-
-        </style>
-    </head>
-    <body>
-      <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!-- Include the above in your HEAD tag ---------->
-
-<!-- If you don't see it u probably are using a browser not based on webkit, so leave IE and grab anything else (Y) -->
-<!-- UPDATE: works in Chrome & Safari, not Firefox. To solve that you could use an SVG insted of pure text. -->
+@extends('layout.master')
+@section('content')
 
 <div class="container">
-<div class="title">MEDICHECK</div>
-<div class="subtitle">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth<!--Solo se muestra si existe un usario activo en la sesion-->
-                <a class="btn btn-outline-dark" href="{{ url('/appointments') }}">Inicio</a>
-            @else
-                <a class="btn btn-outline-dark" href="{{ route('login') }}">Iniciar Sesión</a>
-                <a class="btn btn-outline-dark" href="{{ route('register') }}">Registrarse</a>
-            @endauth
-        </div>
-    @endif
+  
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="/images/imagen1.jpg" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id placerat libero</h1>
+          </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="/images/imagen2.jpg" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h1>Suspendisse potenti. In ut sagittis lacus</h1>
+          </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="/images/imagen3.jpg" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+            <h1>Sed eget molestie ex, eu ornare nulla</h1>
+          </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <br>
+  <h4><center>NOTICIAS</center></h4> <!--Sección de noticias -->
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m12 l3">
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+      </div>
+      
+      <div class="col s12 m12 l3">
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+      </div>
+      
+      <div class="col s12 m12 l3">
+          <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="..." alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+      </div>
+      
+    </div>
+  </div>
+
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="http://localhost:8000/"> medicheck.cl</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
 </div>
-</div><!--Efecto estetico al mover el mouse a traves de la pagina-->
+
+@endsection
+
 <script>
-
-
-  $(document).ready(function(){
-var mouseX, mouseY;
-var ww = $( window ).width();
-var wh = $( window ).height();
-var traX, traY;
-$(document).mousemove(function(e){
-  mouseX = e.pageX;
-  mouseY = e.pageY;
-  traX = ((4 * mouseX) / 570) + 40;
-  traY = ((4 * mouseY) / 570) + 50;
-  console.log(traX);
-  $(".title").css({"background-position": traX + "%" + traY + "%"});
-});
-});
-
+  <script src={{ asset('js/home.js') }}></script>
 </script>
-    </body>
-</html>
